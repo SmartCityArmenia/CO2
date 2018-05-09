ef2afa70d8392a31f5ba610aa6290123e30abec9
SmartyCityArmenia <info@smartcity.am>
2018-05-09 15:51:00 +0400

    V_2.0

---
+++ MySQL2txt.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import pymysql
+import time
+
+conn = pymysql.connect("212.34.238.190","root","my_password","iot" )
+cur = conn.cursor()
+cur.execute("SELECT * FROM `sensors`")
+print(cur.description)
+
+print()
+
+file = open('CO_'+time.strftime("%d-%m-%Y")+'.txt',mode = 'w')
+
+for row in cur:
+    #print(str(row[2])[:-2] + "\t" + str(row[3])[:-2] + "\r")
+    file.write(str(row[2])[:-2] + "\t" + str(row[3])[:-2] + "\r")
+file.close()
+cur.close()
+conn.close()
